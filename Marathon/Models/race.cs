fbc09d4a6f47205ae83351d298836ea94163f364
Jessica Smith <jduesing23@gmail.com>
2024-11-10 07:25:05 -0800

    Marathon API

---
+++ Marathon/Models/race.cs
@@ -0,0 +1,17 @@
+namespace Marathon.Models;
+
+public class RaceCollection
+{
+    public race[] races { get; set; }
+}
+
+public class race
+{
+    public int id {
+        get;
+        set;
+    }
+
+    public string race_name { get; set; }
+    
+}
